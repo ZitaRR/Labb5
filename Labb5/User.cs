b8f0663f7b71c5c3dfaa90d465a17b229785d941
ZitaRR <Haeggqvist1998@hotmail.com>
2019-10-21 10:38:07 +0200

    Added user class

---
+++ Labb5/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb5
+{
+    public sealed class User
+    {
+        public static int TotalUsers = 0;
+
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public int ID { get; private set; }
+
+        public User(string name, string email)
+        {
+            TotalUsers++;
+            ID = TotalUsers;
+
+            Name = name;
+            Email = email;
+        }
+    }
+}
